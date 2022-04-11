05127e998fd9684574f64d86a7f32e3226813c42
Tramnvph14967 <94088002+Tramnvph14967@users.noreply.github.com>
2022-04-11 21:59:09 +0700

    update

---
+++ src/api/order.tsx
@@ -0,0 +1,18 @@
+import instance from "./instance";
+
+export const createOrder = (order : any) =>{
+    const url ="/orders";
+    return instance.post(url,order)
+}
+export const listOrder = () =>{
+    const url ="/orders";
+    return instance.get(url)
+}
+export const updateOrder = (id:any,status : any) =>{
+    const url = `/order/${id}`;
+    return instance.put(url,status)
+};
+export const read = (id:any) =>{
+    const url = `/orders?username=${id}`;
+    return instance.get(url)
+};
